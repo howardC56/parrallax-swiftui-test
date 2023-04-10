75e9d5adf6b5f9c9315818708804c35668fc82b2
howardC56 <h2chang56@gmail.com>
2023-04-10 17:37:39 -0400

    Initial Commit

---
+++ 3d parallax card swiftui/_d_parallax_card_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  _d_parallax_card_swiftuiApp.swift
+//  3d parallax card swiftui
+//
+//  Created by Howard Chang on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct _d_parallax_card_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
